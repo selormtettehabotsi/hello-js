731af2c772dc800b38fd0fd740141ecb3a7a31c5
selormtettehabotsi <tettehash3@gmail.com>
2025-11-05 11:00:34 +0000

    Updated Introduction

---
+++ Intro.js
@@ -0,0 +1,10 @@
+const mainUser = {
+        name: "Selorm",
+        age: 19,
+        favProgrammingLang: "C++",
+    }
+
+  //log the name, age and language respectively onto the terminal  
+console.log("My Name is" + ' ' + mainUser.name);
+console.log("I am " + mainUser.age + " years old.");
+console.log("My favorite  Programming Language is " + mainUser.favProgrammingLang);
